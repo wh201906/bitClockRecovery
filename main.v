3823c52f9c9a18036c17a3af2e18a420a4d1ae8c
wh201906 <wh201906@yandex.com>
2020-09-02 18:41:11 +0800

    freq recovery successed, phase sync with 50% jitter

--- main.v
+++ main.v
@@ -7,12 +7,13 @@ module main (
     
     wire clk_200M;
     
+    reg curr_clk_reset_flag=0;
     reg interval_reg;
     reg [15:0] interval_counter;
 
     reg [15:0] curr_clk_counter;
 
-    reg [1:0] curr_clk_duration_counter;
+    reg [7:0] curr_clk_duration_counter;
 
     clkGen clkGen( // generate 200Mhz clock as the base clock
         .inclk0(clk_50M),
@@ -28,19 +29,26 @@ module main (
             else // edge, but the interval will not change
                 curr_clk_duration_counter=curr_clk_duration_counter+1; // add duratino
             interval_counter=0;
-            if(curr_clk_duration_counter==2'b11) begin // try to add threshold after 4096 edges
+            if(curr_clk_duration_counter==8'b11111111) begin // try to add threshold after 4096 edges
                 clk_freq=clk_freq+1;
                 curr_clk_duration_counter=0;
             end
+            curr_clk_reset_flag=1;
         end
-        else
+        else begin
             interval_counter=interval_counter+1;
+            curr_clk_reset_flag=0;
+        end
         interval_reg=signal;
     end
 
     always @(posedge clk_200M) begin // clock out
         curr_clk_counter=curr_clk_counter+1;
         if(curr_clk_counter>=(clk_freq/2)) begin
+            curr_clk_counter=0;
+            clk_rec=!clk_rec;
+        end
+        else if(curr_clk_reset_flag==1 && curr_clk_counter>=((clk_freq/4))) begin
             curr_clk_counter=0;
             clk_rec=!clk_rec;
         end
