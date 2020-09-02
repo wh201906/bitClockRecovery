638470d079824611fee61982a45437e29c5cde9d
wh201906 <wh201906@yandex.com>
2020-09-02 18:48:18 +0800

    freq recovery successed, phase sync with 25% jitter, clock might distort

--- main.v
+++ main.v
@@ -47,7 +47,7 @@ module main (
             curr_clk_counter=0;
             clk_rec=!clk_rec;
         end
-        else if(curr_clk_reset_flag==1 && curr_clk_counter>=((clk_freq/4))) begin
+        else if(curr_clk_reset_flag==1 && curr_clk_counter>=((clk_freq/8))) begin
             curr_clk_counter=0;
             clk_rec=!clk_rec;
         end
