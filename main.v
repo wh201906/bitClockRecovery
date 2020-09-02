efd80907da2cbae0cea41e2f6708a9ad2b86efd4
wh201906 <wh201906@yandex.com>
2020-09-02 14:53:16 +0800

    freq recovery successed, no phase sync, simplified one

--- main.v
+++ main.v
@@ -1,11 +1,8 @@
 module main (
     input clk_50M,
-    output clk_50M_o,
     input signal,
-    output signal_o,
-    output clk_200M_o,
-    output clk_rec,
-    output [15:0] clk_freq
+    output reg clk_rec,
+    output reg [15:0] clk_freq
 );
     
     wire clk_200M;
@@ -13,9 +10,8 @@ module main (
     reg interval_reg;
     reg [15:0] interval_counter;
 
-    reg [15:0] curr_clk_threshold;
     reg [15:0] curr_clk_counter;
-    reg curr_clk;
+
     reg [11:0] curr_clk_duration_counter;
 
     clkGen clkGen( // generate 200Mhz clock as the base clock
@@ -25,15 +21,15 @@ module main (
     
     always @(posedge clk_200M) begin
         if(interval_reg!=signal) begin //edge detected
-            if(interval_counter<curr_clk_threshold) begin // get the smallest interval
-                curr_clk_threshold=interval_counter; // put the smallest interval
+            if(interval_counter<clk_freq) begin // get the smallest interval
+                clk_freq=interval_counter; // put the smallest interval
                 curr_clk_duration_counter=0; // duration set to 0 since the interval has been updated
             end
             else // edge, but the interval will not change
                 curr_clk_duration_counter=curr_clk_duration_counter+1; // add duratino
             interval_counter=0;
             if(curr_clk_duration_counter==12'hfff) begin // try to add threshold after 4096 edges
-                curr_clk_threshold=curr_clk_threshold+1;
+                clk_freq=clk_freq+1;
 //                curr_clk_duration_counter=0;
             end
         end
@@ -44,19 +40,10 @@ module main (
 
     always @(posedge clk_200M) begin // clock out
         curr_clk_counter=curr_clk_counter+1;
-        if(curr_clk_counter>=curr_clk_threshold) begin
+        if(curr_clk_counter>=clk_freq) begin
             curr_clk_counter=0;
-            curr_clk=!curr_clk;
+            clk_rec=!clk_rec;
         end
-        
     end
-    
-    
-    assign clk_50M_o=clk_50M;
-    assign clk_rec=curr_clk;
-    assign clk_freq=curr_clk_threshold;
-    assign clk_200M_o=clk_200M;
-    assign signal_o=signal;
 
-    
 endmodule
