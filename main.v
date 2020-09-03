1aa6c0c8b8f15a4375965977c178cfb4ef8905aa
wh201906 <wh201906@yandex.com>
2020-09-03 09:36:27 +0800

    Add lock state, higher jitter

--- main.v
+++ main.v
@@ -4,7 +4,8 @@ module main (
     input key_reverse,
     
     output reg clk_rec,
-    output reg [15:0] clk_freq=16'd801
+    output reg [15:0] clk_freq=16'd801,
+    output reg clk_locked=0
 );
     
     wire clk_200M;
@@ -19,6 +20,8 @@ module main (
     reg [3:0] curr_clk_duration_counter;
     
     reg key_rev_reg=1;
+    
+    reg [31:0] clk_locked_operation;
 
     clkGen clkGen( // generate 200Mhz clock as the base clock
         .inclk0(clk_50M),
@@ -36,11 +39,16 @@ module main (
             if(interval_counter<clk_freq) begin // get the smallest interval
                 clk_freq=interval_counter; // put the smallest interval
                 curr_clk_duration_counter=0; // duration set to 0 since the interval has been updated
+                
+                //00 implies that the threshold is changing.
+                clk_locked=(clk_locked_operation!=32'd0);
+                clk_locked_operation={clk_locked_operation[30:0],1'b0};
             end
-            else // edge, but the interval will not change
-                curr_clk_duration_counter=curr_clk_duration_counter+1; // add duratino
+            else begin // edge, but the interval will not change
+                curr_clk_duration_counter=curr_clk_duration_counter+1; // add duration
+            end
             interval_counter=0;
-            if(curr_clk_duration_counter==4'b1111) begin // try to add threshold after 4096 edges
+            if(curr_clk_duration_counter==4'b1111) begin // try to add threshold after 16 edges
                 clk_freq=clk_freq+1;
             end
             curr_clk_reset_flag=1;
@@ -48,6 +56,10 @@ module main (
         else begin
             interval_counter=interval_counter+1;
             curr_clk_reset_flag=0;
+            
+            //11 implies that the threshold is changing.
+            clk_locked=(clk_locked_operation!=32'hffffffff);
+            clk_locked_operation={clk_locked_operation[30:0],1'b1};
         end
         interval_reg=signal;
     end
@@ -61,7 +73,7 @@ module main (
         else if(curr_clk_reset_flag==1 && curr_clk_counter<(clk_freq/8)) begin
             curr_clk_counter=0;
         end
-        if(key_rev_out==0 &&key_rev_reg!=key_rev_out) begin
+        if(key_rev_out==0 && key_rev_reg!=key_rev_out) begin
             clk_rec=!clk_rec;
         end
         key_rev_reg=key_rev_out;
