81b58a13cea14ce5d34e1defd8bc558c84718011
wh201906 <wh201906@yandex.com>
2020-09-05 18:56:51 +0800

    Optimize code

--- main.v
+++ main.v
@@ -5,25 +5,28 @@ module main(
     input key_type,
     
     output reg clk_rec,
-    output reg [15:0] clk_freq=16'd801,
+    output reg [CLK_LEN-1:0] clk_freq,
     output test_led
 );
+
+    localparam CLK_LEN=16;
+    localparam STABLE_LEN=4;
     reg signal_reg;
     reg key_type_reg;
-    reg type_reg=0;
+    reg type_reg=1'b0;
     
     wire clk_300M;
     wire clk_300M_global;
     wire key_rev_out;
     wire key_type_out;
     
-    reg [15:0] interval_counter;
+    reg [CLK_LEN-1:0] interval_counter;
     
     reg clk_reset_flag=1'b0;
 
-    reg [15:0] clk_counter;
+    reg [CLK_LEN-1:0] clk_counter;
 
-    reg [3:0] clk_stable_counter;
+    reg [STABLE_LEN-1:0] clk_stable_counter;
     
     reg key_rev_reg=1'b1;
 
@@ -53,43 +56,43 @@ module main(
         if(signal_reg!=signal) begin //edge detected
             if(interval_counter<clk_freq) begin // get the smallest interval
                 clk_freq=interval_counter; // put the smallest interval
-                clk_stable_counter=4'd0; // duration set to 0 since the interval has been updated
+                clk_stable_counter=0; // duration set to 0 since the interval has been updated
                 
             end
             else begin // edge, but the interval will not change
-                clk_stable_counter=clk_stable_counter+4'd1; // add duration
+                clk_stable_counter++; // add duration
             end
-            interval_counter=16'd0;
-            if(clk_stable_counter==4'b1111) begin // try to add threshold after 16 edges
-                clk_freq=clk_freq+16'd1;
+            interval_counter=0;
+            if(clk_stable_counter=={STABLE_LEN{1'b1}}) begin // try to add threshold after 16 edges
+                clk_freq++;
             end
             clk_reset_flag=1'b1;
         end
         else begin
-            interval_counter=interval_counter+16'd1;
+            interval_counter++;
             clk_reset_flag=1'b0;
         end
         signal_reg=signal;
     end
 
     always @(posedge clk_300M_global) begin // clock out
-        clk_counter=clk_counter+16'd1;
-        if(((clk_counter>=(clk_freq/2)) && type_reg==0) || (clk_counter>=clk_freq && type_reg==1)) begin
-            clk_counter=16'd0;
-            clk_rec=!clk_rec;
+        clk_counter++;
+        if(((clk_counter>=(clk_freq>>1)) && type_reg==1'b0) || (clk_counter>=clk_freq && type_reg==1'b1)) begin
+            clk_counter=0;
+            clk_rec=~clk_rec;
         end
-        else if(clk_reset_flag==1'b1 && clk_counter<(clk_freq/8)) begin
-            clk_counter=16'd0;
+        else if(clk_reset_flag==1'b1 && clk_counter<(clk_freq>>3)) begin
+            clk_counter=0;
         end
-        if(key_rev_out==0 && key_rev_reg!=key_rev_out) begin
-            clk_rec=!clk_rec;
+        if(key_rev_out==1'b0 && key_rev_reg!=key_rev_out) begin
+            clk_rec=~clk_rec;
         end
         key_rev_reg=key_rev_out;
     end
     
     always @(posedge clk_300M_global) begin
-        if(key_type_out==0 && key_type_reg!=key_type_out) begin
-            type_reg=!type_reg;
+        if(key_type_out==1'b0 && key_type_reg!=key_type_out) begin
+            type_reg=~type_reg;
         end
         key_type_reg=key_type_out;
     end
